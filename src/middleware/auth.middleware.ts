c44e8dcfce07c5d71b66b7a983c78c9946ab57eb
m00nk0d3 <joaopbmartins1981@gmail.com>
2025-09-29 09:01:33 +0000

    fix: type error on middleware

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,6 +1,6 @@
 import { AuthContext } from "../context/auth.context";
 
-export function checkAuth(ctx) {
+export function checkAuth(ctx: AuthContext) {
   if (!ctx.user) throw new Error("Authentication required");
   console.log("testing husky");
 }
