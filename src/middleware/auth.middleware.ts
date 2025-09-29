7b602f2f2863264055c41b4cb93906889140c174
m00nk0d3 <joaopbmartins1981@gmail.com>
2025-09-29 07:31:38 +0000

    Add console log for testing in authentication middleware

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -2,4 +2,5 @@ import { AuthContext } from "../context/auth.context";
 
 export function checkAuth(ctx: AuthContext) {
   if (!ctx.user) throw new Error("Authentication required");
+  console.log("testing husky");
 }
