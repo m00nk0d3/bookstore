cd3473cf94cc6df6f0a6a011d1287d567060c8c1
m00nk0d3 <joaopbmartins1981@gmail.com>
2025-10-05 08:06:44 +0000

    cleaning up

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -2,5 +2,4 @@ import { AuthContext } from "../context/auth.context";
 
 export function checkAuth(ctx: AuthContext) {
   if (!ctx.user) throw new Error("Authentication required");
-  console.log("testing husky");
 }
